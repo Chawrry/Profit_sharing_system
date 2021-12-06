d2fbcb8eb7bf21e00d120098831f5965b89f880c
"Chawrry" <"chwat103@gmail.com">
2021-12-06 13:55:28 +0800

    develop investor function

--- contract/Profit_share.sol
+++ contract/Profit_share.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.8.7;
+pragma solidity >=0.8.0 <0.8.7;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+import "https://github.com/abdk-consulting/abdk-libraries-solidity/blob/master/ABDKMath64x64.sol";
 
 contract ProfitShare {
 
@@ -14,12 +15,9 @@ contract ProfitShare {
     address private owner;
     // investor:
     mapping(address => uint) private investorAmountMap;
-    uint public maxClaimableSession;
-    // calcuate current session profit
-    uint public currentSessionProfit;
-    // session profit before expired
-    uint private expiredSessionProfit;
-    uint [] private sessionProfitArray;
+    uint private investorAmountSum;
+    uint public maxClaimableSession = 1;
+    int128 [] private sessionProfitArray;
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -59,43 +57,95 @@ contract ProfitShare {
 
     function sessionStart() public onlyOwner asSessionEnd {
         sessionState = SessionState.START;
+        // add a new profit in the begining of the session.
+        sessionProfitArray.push(0);
     }
 
     function sessionStop() public onlyOwner asSessionStart {
         sessionState = SessionState.END;
-        // record profit
-        sessionProfitArray.push(currentSessionProfit);
-        currentSessionProfit = 0;
+        // recalcuate expired profit in sessionProfitArray in the end of the session. 
+        uint profitArrayMaxIdx = sessionProfitArray.length.sub(1);
+        if (profitArrayMaxIdx > maxClaimableSession) {
+            uint expiredProfitArrayIdx = profitArrayMaxIdx.sub(maxClaimableSession);
+            sessionProfitArray[expiredProfitArrayIdx] = 0;
+        }
     }
 
-    function addProfit(uint profit) public onlyOwner asSessionStart {
-        currentSessionProfit = currentSessionProfit.add(profit);
+    function addProfit(int128 profit) public onlyOwner asSessionStart {
+        require(profit > 0);
+
+        sessionProfitArray[sessionProfitArray.length - 1] = profit;
     }
 
 
     /* everyone */
 
     function invest(address investor, uint amount) public asSessionStart {
+        require(amount > 0);
+
         investorAmountMap[investor] = investorAmountMap[investor].add(amount);
+        investorAmountSum = investorAmountSum.add(amount);
+    }
+
+    function getCurrentSession() public view returns(uint) {
+        return sessionProfitArray.length;
     }
 
     
     /* onlyInvestors */
 
     function withdraw(address investor, uint amount) public onlyInvestors asSessionStart {
+        require(amount > 0);
+        require(investorAmountMap[investor] > amount);
+
         investorAmountMap[investor] = investorAmountMap[investor].sub(amount);
+        investorAmountSum = investorAmountSum.sub(amount);
     }
 
     function claim() public onlyInvestors asSessionStart {
         // get expiredSessionProfit
-        // share expiredSessionProfit by invest rate
-        // recalculate expiredSessionProfit
-        // refresh sessionProfitArray
+        int128 expiredProfit = getExpiredSessionProfit();
+
+        // get the investment ratio
+        int128 expiredPrfoitRate = ABDKMath64x64.divu(investorAmountMap[msg.sender], investorAmountSum);
+        int128 claimProfit = ABDKMath64x64.mul(expiredProfit, expiredPrfoitRate);
+
+        // reload sessionProfitArray
+        reloadSessionProfitArray(claimProfit);
     }
 
-    // cal expiredSessionProfit
-    function calExpriedSessionProfit() private {
-    
+
+
+    function getExpiredSessionProfit() internal view returns(int128) {
+        int128 result = 0;
+        uint profitArrayMaxIdx = sessionProfitArray.length.sub(1);
+        uint profitArrayMinIdx = 0;
+        if (profitArrayMaxIdx > maxClaimableSession) {
+            profitArrayMinIdx = profitArrayMaxIdx.sub(maxClaimableSession);
+        }
+         for (uint i=profitArrayMinIdx; i<=profitArrayMaxIdx; i++) {
+            result = ABDKMath64x64.add(result ,sessionProfitArray[i]);
+        }
+
+        return result;
+    }
+
+    function reloadSessionProfitArray(int128 claimProfit) internal {
+        uint profitArrayMaxIdx = sessionProfitArray.length.sub(1);
+        uint profitArrayMinIdx = 0;
+        if (profitArrayMaxIdx > maxClaimableSession) {
+            profitArrayMinIdx = profitArrayMaxIdx.sub(maxClaimableSession);
+        }
+        for (uint i=profitArrayMinIdx; claimProfit==0 || i<=profitArrayMaxIdx; i++) {
+            int128 sessionProfit = sessionProfitArray[i];
+            if (claimProfit > sessionProfit) {
+                sessionProfitArray[i] = 0;
+                claimProfit = ABDKMath64x64.sub(claimProfit, sessionProfit);
+            } else {
+                sessionProfitArray[i] = ABDKMath64x64.sub(sessionProfit, claimProfit);
+                claimProfit = 0;
+            }
+        }
     }
 
 }
